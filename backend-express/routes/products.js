4628d37cf50b2afa3ee8ebfd2981d7bcbea1c779
ngys9919 <ngys9919@yahoo.com>
2024-11-27 22:00:31 +0800

    implement CRUD for books

--- backend-express/routes/products.js
+++ backend-express/routes/products.js
@@ -12,10 +12,64 @@ router.get('/', async (req, res) => {
   }
 });
 
+//GET => Read
 // GET a single product
 router.get('/:id', async (req, res) => {
   try {
     const product = await productService.getProductById(req.params.id);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+//POST => Create
+// POST a single product
+router.post('/create', async (req, res) => {
+  try {
+    let {isbn_10, isbn_13, bookTitle, pageCount, priceTag, image, format, promotion, badge, discount, review, firstName, lastName} = req.body;
+    
+    // basic validation: 
+    // make sure that bookTitle, isbn_13 and firstName must be present
+    if (!firstName || !isbn_13 || !bookTitle) {
+      return res.status(400)
+      .json({ "error": "The field(s) is incomplete: isbn_10, isbn_13, bookTitle, pageCount, priceTag, image, format, promotion, badge, discount, review, firstName, lastName" })
+    }
+
+    
+
+    const product = await productService.createProductByBody(req.body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+//PUT => Update by replace
+// PUT a single product
+router.put('/:id/edit', async (req, res) => {
+  try {
+    let {isbn_10, isbn_13, bookTitle, pageCount, priceTag, image, format, promotion, badge, discount, review, firstName, lastName} = req.body;
+    
+    // basic validation: 
+    // make sure that bookTitle, isbn_13 and firstName must be present
+    if (!firstName || !isbn_13 || !bookTitle) {
+      return res.status(400)
+      .json({ "error": "The field(s) is incomplete: isbn_10, isbn_13, bookTitle, pageCount, priceTag, image, format, promotion, badge, discount, review, firstName, lastName" })
+    }
+
+    const product = await productService.updateProductByIdBody(req.params.id, req.body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+//DELETE => Delete
+// DELETE a single product
+router.delete('/:id/delete', async (req, res) => {
+  try {
+    const product = await productService.deleteProductById(req.params.id);
     res.json(product);
   } catch (error) {
     res.status(404).json({ message: error.message });
